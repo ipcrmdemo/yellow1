f7cf6d4343ede46e8ecceb436282af7c7a984793
Matt Cadorette <mcadorette@gmail.com>
2018-09-25 15:27:35 -0400

    Initial commit from Atomist

---
+++ src/main/java/yellow1/Yellow1Application.java
@@ -0,0 +1,12 @@
+package yellow1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Yellow1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Yellow1Application.class, args);
+	}
+}
---
+++ src/test/java/yellow1/Yellow1ApplicationTests.java
@@ -0,0 +1,16 @@
+package yellow1;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Yellow1ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
